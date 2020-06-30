00be2f044d489d18a359ec2708ad4a969bce810f
Aaron <tim2464@yahoo.com.tw>
2020-06-29 22:04:51 -0700

    Rolling stateful dice using setState function

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +17,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: genRandomNum,
+              child: Image.asset('images/dice$leftDiceNum.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: genRandomNum,
+              child: Image.asset('images/dice$rightDiceNum.png'),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void genRandomNum() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
   }
 }
