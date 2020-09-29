6e0bbe847d2d01c190164bb7ac44858ca9559ab3
Aaron <tim2464@yahoo.com.tw>
2020-09-29 12:17:18 -0700

    Add http package and NetworkHelper

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 
 import '../services/location.dart';
@@ -8,17 +9,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=b6907d289e10d714a6e88b30761fae22');
+    var weatherData = await networkHelper.getData();
   }
 
   @override
@@ -27,7 +34,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            getLocationData();
             //Get the current location
           },
           child: Text('Get Location'),
