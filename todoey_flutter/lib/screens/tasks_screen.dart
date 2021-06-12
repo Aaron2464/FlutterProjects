67a52672529375a6e86e08c1fc4a7e32f9c91370
Aaron <tim2464@yahoo.com.tw>
2021-06-11 18:38:12 -0700

    Create bottomsheet

--- todoey_flutter/lib/screens/tasks_screen.dart
+++ todoey_flutter/lib/screens/tasks_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 import 'package:todoey_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
-  const TasksScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (context) => AddTaskScreen());
+        },
         child: Icon(Icons.add),
       ),
       body: Column(
